0a675f5dd44ea8fe2031d394126c6bc9d9bb691b
Bastien Grasnick <bastien.grasnick@deutschebahn.com>
2023-08-29 18:12:43 +0200

    Test: add tests for SCC allows
    Signed-off-by: Bastien Grasnick <bastien.grasnick@deutschebahn.com>

--- pkg/components/exporter/exporter_test.go
+++ pkg/components/exporter/exporter_test.go
@@ -3,11 +3,22 @@ package exporter
 import (
 	"testing"
 
+	secv1 "github.com/openshift/api/security/v1"
 	"github.com/stretchr/testify/assert"
 	"github.com/sustainable.computing.io/kepler-operator/pkg/api/v1alpha1"
+	"github.com/sustainable.computing.io/kepler-operator/pkg/components"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 )
+
+type SCCAllows struct {
+	AllowPrivilegedContainer bool
+	AllowHostDirVolumePlugin bool
+	AllowHostIPC             bool
+	AllowHostNetwork         bool
+	AllowHostPID             bool
+	AllowHostPorts           bool
+}
 
 func NodeSelectorFromDS(ds *appsv1.DaemonSet) map[string]string {
 	return ds.Spec.Template.Spec.NodeSelector
@@ -23,6 +34,17 @@ func HostPIDFromDS(ds *appsv1.DaemonSet) bool {
 
 func VolumeMountsFromDS(ds *appsv1.DaemonSet) []corev1.VolumeMount {
 	return ds.Spec.Template.Spec.Containers[0].VolumeMounts
+}
+
+func AllowsFromSCC(SCC *secv1.SecurityContextConstraints) SCCAllows {
+	return SCCAllows{
+		AllowPrivilegedContainer: SCC.AllowPrivilegedContainer,
+		AllowHostDirVolumePlugin: SCC.AllowHostDirVolumePlugin,
+		AllowHostIPC:             SCC.AllowHostIPC,
+		AllowHostNetwork:         SCC.AllowHostNetwork,
+		AllowHostPID:             SCC.AllowHostPID,
+		AllowHostPorts:           SCC.AllowHostPorts,
+	}
 }
 
 func TestNodeSelection(t *testing.T) {
@@ -153,6 +175,41 @@ func TestVolumeMounts(t *testing.T) {
 			}
 			actual := VolumeMountsFromDS(NewDaemonSet(&k))
 			assert.Equal(t, actual, tc.volumeMounts)
+		})
+	}
+}
+
+func TestSCCAllowHostIPCIsFalse(t *testing.T) {
+	tt := []struct {
+		spec      v1alpha1.ExporterSpec
+		sccAllows SCCAllows
+		scenario  string
+	}{
+		{
+			spec: v1alpha1.ExporterSpec{},
+			sccAllows: SCCAllows{
+				AllowPrivilegedContainer: true,
+				AllowHostDirVolumePlugin: true,
+				AllowHostIPC:             false,
+				AllowHostNetwork:         false,
+				AllowHostPID:             true,
+				AllowHostPorts:           false,
+			},
+			scenario: "default case",
+		},
+	}
+
+	for _, tc := range tt {
+		tc := tc
+		t.Run(tc.scenario, func(t *testing.T) {
+			t.Parallel()
+			k := v1alpha1.Kepler{
+				Spec: v1alpha1.KeplerSpec{
+					Exporter: tc.spec,
+				},
+			}
+			actual := AllowsFromSCC(NewSCC(components.Full, &k))
+			assert.Equal(t, actual, tc.sccAllows)
 		})
 	}
 }
