5f6e191f2d1679d9c5eb3a5eedf09d42116a27b7
Vibhu Prashar <vibhu.sharma2929@gmail.com>
2024-07-11 12:56:43 +0530

    chore: bump up Kepler to 0.7.11
    This commit bumps up Kepler version to 0.7.11
    
    Signed-off-by: Vibhu Prashar <vibhu.sharma2929@gmail.com>

--- tests/e2e/kepler_internal_test.go
+++ tests/e2e/kepler_internal_test.go
@@ -35,7 +35,7 @@ import (
 )
 
 const (
-	keplerImage = `quay.io/sustainable_computing_io/kepler:release-0.7.10`
+	keplerImage = `quay.io/sustainable_computing_io/kepler:release-0.7.11`
 )
 
 func TestKeplerInternal_Reconciliation(t *testing.T) {
