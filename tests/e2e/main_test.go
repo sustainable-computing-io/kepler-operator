f80eec93f90b8f2f646e43aba2eb1e1ddd739fff
Sunil Thaha <sthaha@redhat.com>
2025-10-02 21:51:04 +1000

    chore(kepler): bump up version to 0.11.2
    Signed-off-by: Sunil Thaha <sthaha@redhat.com>

--- tests/e2e/main_test.go
+++ tests/e2e/main_test.go
@@ -13,7 +13,7 @@ import (
 )
 
 const (
-	keplerImage        = `quay.io/sustainable_computing_io/kepler:v0.11.0`
+	keplerImage        = `quay.io/sustainable_computing_io/kepler:v0.11.2`
 	kubeRbacProxyImage = `quay.io/brancz/kube-rbac-proxy:v0.19.0`
 )
 
