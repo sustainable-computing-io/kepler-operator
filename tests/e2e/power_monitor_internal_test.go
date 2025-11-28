d61fc46f6ace8d3b671e44a5f1a721af75d175f2
vprashar2929 <vibhu.sharma2929@gmail.com>
2025-11-28 15:39:16 +0530

    test: migrate to ginkgo
    Signed-off-by: vprashar2929 <vibhu.sharma2929@gmail.com>

--- tests/e2e/power_monitor_internal_test.go
+++ tests/e2e/power_monitor_internal_test.go
@@ -5,11 +5,10 @@ package e2e
 
 import (
 	"fmt"
-	"strings"
-	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 	"github.com/sustainable.computing.io/kepler-operator/api/v1alpha1"
 	"github.com/sustainable.computing.io/kepler-operator/internal/controller"
 	powermonitor "github.com/sustainable.computing.io/kepler-operator/pkg/components/power-monitor"
@@ -19,168 +18,186 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
-func TestPowerMonitorInternal_Reconciliation(t *testing.T) {
-	f := utils.NewFramework(t)
-	name := "e2e-pmi"
-	testNs := controller.PowerMonitorDeploymentNS
+var _ = Describe("PowerMonitorInternal", func() {
+	var f *utils.Framework
 
-	// Pre-condition: Verify PowerMonitorInternal doesn't exist
-	f.AssertNoResourceExists(name, "", &v1alpha1.PowerMonitorInternal{})
-
-	// Create PowerMonitorInternal
-	f.CreateTestPowerMonitorInternal(name, testNs, runningOnVM, testKeplerImage, testKubeRbacProxyImage, Cluster, v1alpha1.SecurityModeNone, []string{})
-
-	// Verify namespace exists
-	f.AssertResourceExists(testNs, "", &corev1.Namespace{})
-	ds := appsv1.DaemonSet{}
-
-	// Wait for PowerMonitorInternal to be reconciled
-	pmi := f.WaitUntilPowerMonitorInternalCondition(name, v1alpha1.Reconciled, v1alpha1.ConditionTrue)
-
-	// Verify daemonset exists and has correct configuration
-	f.AssertResourceExists(pmi.Name, testNs, &ds)
-	containers := ds.Spec.Template.Spec.Containers
-	assert.Equal(t, 1, len(containers))
-	assert.Equal(t, 1, len(containers[0].Ports))
-	assert.EqualValues(t, 28282, containers[0].Ports[0].ContainerPort)
-
-	// Verify PowerMonitorInternal status
-	f.AssertPowerMonitorInternalStatus(pmi.Name, utils.Timeout(5*time.Minute))
-}
-
-func TestPowerMonitorInternal_RBAC_Reconciliation(t *testing.T) {
-	f := utils.NewFramework(t)
-	name := "e2e-pmi"
-	// test namespace must be the deployment namespace for controller
-	// to watch the deployments / daemonsets etc
-	testNs := controller.PowerMonitorDeploymentNS
-
-	// pre-condition
-	f.AssertNoResourceExists(name, "", &v1alpha1.PowerMonitorInternal{})
-
-	// Create PowerMonitorInternal with RBAC security mode
-	pmi := f.CreateTestPowerMonitorInternal(name, testNs, runningOnVM, testKeplerImage, testKubeRbacProxyImage, Cluster, v1alpha1.SecurityModeRBAC, []string{
-		"successful-test-namespace:successful-test-curl-sa",
+	BeforeEach(func() {
+		f = utils.NewFramework()
 	})
 
-	tlsCertSecretName := powermonitor.SecretTLSCertName
-	var caCertSource string
+	Describe("Reconciliation", func() {
+		Context("when creating a basic PowerMonitorInternal", func() {
+			It("should reconcile and create DaemonSet", func() {
+				name := "e2e-pmi"
+				testNs := controller.PowerMonitorDeploymentNS
 
-	if Cluster == k8s.Kubernetes {
-		// For Kubernetes clusters, deploy cert-manager and dependencies
-		clusterIssuerName := "selfsigned-cluster-issuer"
-		caCertName := "power-monitor-ca"
-		caCertSecretName := "power-monitor-ca-secret"
-		pmIssuerName := "power-monitor-ca-issuer"
-		f.DeployOpenshiftCerts(
-			pmi.Name,
-			testNs,
-			clusterIssuerName,
-			caCertName,
-			caCertSecretName,
-			pmIssuerName,
-			tlsCertSecretName,
-			tlsCertSecretName,
-		)
-		caCertSource = caCertSecretName
-	} else {
-		f.WaitUntilPowerMonitorInternalCondition(pmi.Name, v1alpha1.Reconciled, v1alpha1.ConditionTrue)
-		f.WaitForOpenshiftCerts(pmi.Name, testNs, tlsCertSecretName)
-		caCertSource = tlsCertSecretName
-	}
+				By("verifying PowerMonitorInternal doesn't exist initially")
+				f.ExpectNoResourceExists(name, "", &v1alpha1.PowerMonitorInternal{})
 
-	// then
-	f.AssertResourceExists(testNs, "", &corev1.Namespace{})
-	ds := appsv1.DaemonSet{}
-	f.AssertResourceExists(pmi.Name, testNs, &ds)
+				By("creating PowerMonitorInternal")
+				f.CreateTestPowerMonitorInternal(name, testNs, runningOnVM, testKeplerImage, testKubeRbacProxyImage, Cluster, v1alpha1.SecurityModeNone, []string{})
 
-	retrievedPmi := f.WaitUntilPowerMonitorInternalCondition(name, v1alpha1.Reconciled, v1alpha1.ConditionTrue)
-	// default toleration
-	assert.Equal(t, []corev1.Toleration{{Operator: "Exists"}}, retrievedPmi.Spec.Kepler.Deployment.Tolerations)
-	reconciled, err := k8s.FindCondition(retrievedPmi.Status.Conditions, v1alpha1.Reconciled)
-	assert.NoError(t, err, "unable to get reconciled condition")
-	assert.Equal(t, reconciled.ObservedGeneration, retrievedPmi.Generation)
-	assert.Equal(t, reconciled.Status, v1alpha1.ConditionTrue)
+				By("verifying namespace is created")
+				f.ExpectResourceExists(testNs, "", &corev1.Namespace{})
+				ds := &appsv1.DaemonSet{}
 
-	retrievedPmi = f.WaitUntilPowerMonitorInternalCondition(name, v1alpha1.Available, v1alpha1.ConditionTrue)
-	available, err := k8s.FindCondition(retrievedPmi.Status.Conditions, v1alpha1.Available)
-	assert.NoError(t, err, "unable to get available condition")
-	assert.Equal(t, available.ObservedGeneration, retrievedPmi.Generation)
-	assert.Equal(t, available.Status, v1alpha1.ConditionTrue)
+				By("waiting for PowerMonitorInternal to be reconciled")
+				pmi := f.ExpectPowerMonitorInternalCondition(name, v1alpha1.Reconciled, v1alpha1.ConditionTrue)
 
-	audience := fmt.Sprintf("%s.%s.svc", pmi.Name, testNs)
-	serviceURL := fmt.Sprintf(
-		"https://%s.%s.svc:%d/metrics",
-		pmi.Name,
-		testNs,
-		powermonitor.SecurePort,
-	)
+				By("verifying DaemonSet exists and has correct configuration")
+				f.ExpectResourceExists(pmi.Name, testNs, ds)
+				containers := ds.Spec.Template.Spec.Containers
+				Expect(containers).To(HaveLen(1), "should have exactly 1 container")
+				Expect(containers[0].Ports).To(HaveLen(1), "should have exactly 1 port")
+				Expect(containers[0].Ports[0].ContainerPort).To(BeEquivalentTo(28282))
 
-	// wait for relevant secrets to be created
-	tlsSecret := corev1.Secret{}
-	f.AssertResourceExists(
-		tlsCertSecretName,
-		testNs,
-		&tlsSecret,
-		utils.Timeout(5*time.Minute),
-	)
-	assert.NotEmpty(t, tlsSecret.Data["tls.crt"], "TLS cert should be present")
-	assert.NotEmpty(t, tlsSecret.Data["tls.key"], "TLS key should be present")
+				By("verifying PowerMonitorInternal status")
+				f.ExpectPowerMonitorInternalStatus(pmi.Name, utils.Timeout(5*time.Minute))
+			})
+		})
+	})
 
-	// deploy successful curl job
-	successfulJobName := "successful-test-curl"
-	successfulTestSAName := "successful-test-curl-sa"
-	successfulTestCurlNs := "successful-test-namespace"
-	var jobLogs string
+	Describe("RBAC Reconciliation", func() {
+		Context("when using RBAC security mode", func() {
+			It("should reconcile with TLS certificates and access control", func() {
+				name := "e2e-pmi"
+				// test namespace must be the deployment namespace for controller
+				// to watch the deployments / daemonsets etc
+				testNs := controller.PowerMonitorDeploymentNS
 
-	if Cluster == k8s.Kubernetes {
-		jobLogs = f.CreateCurlPowerMonitorTestSuite(
-			successfulJobName,
-			successfulTestSAName,
-			successfulTestCurlNs,
-			audience,
-			serviceURL,
-			caCertSource,
-			testNs,
-		)
-	} else {
-		jobLogs = f.CreateCurlPowerMonitorTestSuiteForOpenShift(
-			successfulJobName,
-			successfulTestSAName,
-			successfulTestCurlNs,
-			audience,
-			serviceURL,
-			caCertSource,
-			testNs,
-		)
-	}
-	assert.True(t, strings.Contains(jobLogs, "HTTP/2 200"), fmt.Sprintf("expected %s to successfully access (200) the secure endpoint but it did not", successfulJobName))
+				By("verifying PowerMonitorInternal doesn't exist initially")
+				f.ExpectNoResourceExists(name, "", &v1alpha1.PowerMonitorInternal{})
 
-	// deploy blocked curl job
-	failedJobname := "failed-test-curl"
-	failedTestSAName := "failed-test-curl-sa"
-	failedTestCurlNs := "failed-test-namespace"
+				By("creating PowerMonitorInternal with RBAC security mode")
+				pmi := f.CreateTestPowerMonitorInternal(name, testNs, runningOnVM, testKeplerImage, testKubeRbacProxyImage, Cluster, v1alpha1.SecurityModeRBAC, []string{
+					"successful-test-namespace:successful-test-curl-sa",
+				})
 
-	if Cluster == k8s.Kubernetes {
-		jobLogs = f.CreateCurlPowerMonitorTestSuite(
-			failedJobname,
-			failedTestSAName,
-			failedTestCurlNs,
-			audience,
-			serviceURL,
-			caCertSource,
-			testNs,
-		)
-	} else {
-		jobLogs = f.CreateCurlPowerMonitorTestSuiteForOpenShift(
-			failedJobname,
-			failedTestSAName,
-			failedTestCurlNs,
-			audience,
-			serviceURL,
-			caCertSource,
-			testNs,
-		)
-	}
-	assert.True(t, strings.Contains(jobLogs, "HTTP/2 403"), fmt.Sprintf("expected %s to receive a forbidden error (403) when attempting to access secure endpoint but did not", failedJobname))
-}
+				tlsCertSecretName := powermonitor.SecretTLSCertName
+				var caCertSource string
+
+				if Cluster == k8s.Kubernetes {
+					By("deploying cert-manager certificates for Kubernetes cluster")
+					clusterIssuerName := "selfsigned-cluster-issuer"
+					caCertName := "power-monitor-ca"
+					caCertSecretName := "power-monitor-ca-secret"
+					pmIssuerName := "power-monitor-ca-issuer"
+					f.DeployOpenshiftCerts(
+						pmi.Name,
+						testNs,
+						clusterIssuerName,
+						caCertName,
+						caCertSecretName,
+						pmIssuerName,
+						tlsCertSecretName,
+						tlsCertSecretName,
+					)
+					caCertSource = caCertSecretName
+				} else {
+					By("waiting for OpenShift certificates")
+					f.WaitUntilPowerMonitorInternalCondition(pmi.Name, v1alpha1.Reconciled, v1alpha1.ConditionTrue)
+					f.WaitForOpenshiftCerts(pmi.Name, testNs, tlsCertSecretName)
+					caCertSource = tlsCertSecretName
+				}
+
+				By("verifying namespace and DaemonSet exist")
+				f.ExpectResourceExists(testNs, "", &corev1.Namespace{})
+				ds := &appsv1.DaemonSet{}
+				f.ExpectResourceExists(pmi.Name, testNs, ds)
+
+				By("verifying PowerMonitorInternal is reconciled")
+				retrievedPmi := f.ExpectPowerMonitorInternalCondition(name, v1alpha1.Reconciled, v1alpha1.ConditionTrue)
+				Expect(retrievedPmi.Spec.Kepler.Deployment.Tolerations).To(Equal([]corev1.Toleration{{Operator: "Exists"}}))
+				reconciled, err := k8s.FindCondition(retrievedPmi.Status.Conditions, v1alpha1.Reconciled)
+				Expect(err).NotTo(HaveOccurred(), "unable to get reconciled condition")
+				Expect(reconciled.ObservedGeneration).To(Equal(retrievedPmi.Generation))
+				Expect(reconciled.Status).To(Equal(v1alpha1.ConditionTrue))
+
+				By("verifying PowerMonitorInternal is available")
+				retrievedPmi = f.ExpectPowerMonitorInternalCondition(name, v1alpha1.Available, v1alpha1.ConditionTrue)
+				available, err := k8s.FindCondition(retrievedPmi.Status.Conditions, v1alpha1.Available)
+				Expect(err).NotTo(HaveOccurred(), "unable to get available condition")
+				Expect(available.ObservedGeneration).To(Equal(retrievedPmi.Generation))
+				Expect(available.Status).To(Equal(v1alpha1.ConditionTrue))
+
+				audience := fmt.Sprintf("%s.%s.svc", pmi.Name, testNs)
+				serviceURL := fmt.Sprintf(
+					"https://%s.%s.svc:%d/metrics",
+					pmi.Name,
+					testNs,
+					powermonitor.SecurePort,
+				)
+
+				By("verifying TLS secrets are created")
+				tlsSecret := &corev1.Secret{}
+				f.ExpectResourceExists(
+					tlsCertSecretName,
+					testNs,
+					tlsSecret,
+					utils.Timeout(5*time.Minute),
+				)
+				Expect(tlsSecret.Data["tls.crt"]).NotTo(BeEmpty(), "TLS cert should be present")
+				Expect(tlsSecret.Data["tls.key"]).NotTo(BeEmpty(), "TLS key should be present")
+
+				By("deploying curl job with allowed service account")
+				successfulJobName := "successful-test-curl"
+				successfulTestSAName := "successful-test-curl-sa"
+				successfulTestCurlNs := "successful-test-namespace"
+				var jobLogs string
+
+				if Cluster == k8s.Kubernetes {
+					jobLogs = f.CreateCurlPowerMonitorTestSuite(
+						successfulJobName,
+						successfulTestSAName,
+						successfulTestCurlNs,
+						audience,
+						serviceURL,
+						caCertSource,
+						testNs,
+					)
+				} else {
+					jobLogs = f.CreateCurlPowerMonitorTestSuiteForOpenShift(
+						successfulJobName,
+						successfulTestSAName,
+						successfulTestCurlNs,
+						audience,
+						serviceURL,
+						caCertSource,
+						testNs,
+					)
+				}
+				Expect(jobLogs).To(ContainSubstring("HTTP/2 200"),
+					"expected %s to successfully access (200) the secure endpoint", successfulJobName)
+
+				By("deploying curl job with blocked service account")
+				failedJobname := "failed-test-curl"
+				failedTestSAName := "failed-test-curl-sa"
+				failedTestCurlNs := "failed-test-namespace"
+
+				if Cluster == k8s.Kubernetes {
+					jobLogs = f.CreateCurlPowerMonitorTestSuite(
+						failedJobname,
+						failedTestSAName,
+						failedTestCurlNs,
+						audience,
+						serviceURL,
+						caCertSource,
+						testNs,
+					)
+				} else {
+					jobLogs = f.CreateCurlPowerMonitorTestSuiteForOpenShift(
+						failedJobname,
+						failedTestSAName,
+						failedTestCurlNs,
+						audience,
+						serviceURL,
+						caCertSource,
+						testNs,
+					)
+				}
+				Expect(jobLogs).To(ContainSubstring("HTTP/2 403"),
+					"expected %s to receive forbidden error (403) when accessing secure endpoint", failedJobname)
+			})
+		})
+	})
+})
